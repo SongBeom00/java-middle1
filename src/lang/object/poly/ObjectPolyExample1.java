63280eed8d37c855aa195be7f48b714e8d46c5e4
SongBeom00 <kjb001023@naver.com>
2025-03-22 20:16:01 +0900

    upcasting & downcasting

--- src/lang/object/poly/ObjectPolyExample1.java
+++ src/lang/object/poly/ObjectPolyExample1.java
@@ -5,7 +5,9 @@ public class ObjectPolyExample1 {
     public static void main(String[] args) {
         Dog dog = new Dog();
         Car car = new Car();
+
         action(dog);
+        action(car);
     }
 
     private static void action(Object obj) {
